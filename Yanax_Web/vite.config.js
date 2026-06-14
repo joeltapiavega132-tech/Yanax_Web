f83ef2aadc08f1bf0909810288027d742b995e5d
joeltapiavega132-tech <joeltapiavega132@gmail.com>
2026-06-14 13:08:22 -0500

    Update vite.config.js

--- Yanax_Web/vite.config.js
+++ Yanax_Web/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './', // <--- AGREGA ESTA LÍNEA AQUÍ
-})
+  // Si está en modo "serve" (localhost) usa '/', si va a producción usa './'
+  base: command === 'serve' ? '/' : './',
+}))
